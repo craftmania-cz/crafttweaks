cd8fe86b8acd4a7030d3588e7736e3612c7534f8
MrWakeCZ <joseff.kral@gmail.com>
2018-03-31 03:04:21 +0200

    Blokace NameTagů na ArmorStand

--- src/main/java/cz/craftmania/crafttweaks/crafttweaks/Main.java
+++ src/main/java/cz/craftmania/crafttweaks/crafttweaks/Main.java
@@ -1,17 +1,30 @@
 package cz.craftmania.crafttweaks.crafttweaks;
 
+import cz.craftmania.crafttweaks.crafttweaks.listeners.FixesListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
+    private static Main instance;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+
+        // Instance
+        instance = this;
+
+        Bukkit.getServer().getPluginManager().registerEvents(new FixesListener(), this);
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
+        instance = null;
+    }
+
+    public static Main getInstance() {
+        return instance;
     }
 }
---
+++ src/main/java/cz/craftmania/crafttweaks/crafttweaks/listeners/FixesListener.java
@@ -0,0 +1,41 @@
+package cz.craftmania.crafttweaks.crafttweaks.listeners;
+
+import org.bukkit.Material;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class FixesListener implements Listener {
+
+    /*
+        Oprava klikani s NameTagy na Armorstandy -> bugovani itemu (duplikace)
+     */
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
+    public void onInteract(PlayerInteractAtEntityEvent e){
+        final Player p = e.getPlayer();
+        final Entity entity = e.getRightClicked();
+        final ItemStack item = e.getPlayer().getInventory().getItemInMainHand();
+
+        if(entity == null){
+            return;
+        }
+
+        if(item == null){
+            return;
+        }
+
+        if(entity instanceof ArmorStand){
+            if(item.getType() == Material.NAME_TAG){
+                p.sendMessage("§cTato akce je docasne blokovana!");
+                e.setCancelled(true);
+            }
+        }
+
+
+    }
+}
