adee030ebfa17153f977153379fd1b6c9f36ec30
MrWakeCZ <joseff.kral@gmail.com>
2020-03-15 02:17:49 +0100

    Spawn entit dle TPS

--- src/main/java/cz/craftmania/crafttweaks/crafttweaks/Main.java
+++ src/main/java/cz/craftmania/crafttweaks/crafttweaks/Main.java
@@ -3,6 +3,7 @@ package cz.craftmania.crafttweaks.crafttweaks;
 import cz.craftmania.crafttweaks.crafttweaks.listeners.*;
 import cz.craftmania.crafttweaks.crafttweaks.listeners.blockers.DisableBlockBreakListener;
 import cz.craftmania.crafttweaks.crafttweaks.listeners.blockers.DisableBlockPlaceListener;
+import cz.craftmania.crafttweaks.crafttweaks.tasks.LagManager;
 import cz.craftmania.crafttweaks.crafttweaks.utils.Logger;
 import cz.craftmania.crafttweaks.crafttweaks.utils.console.ConsoleEngine;
 import cz.craftmania.crafttweaks.crafttweaks.utils.console.EngineInterface;
@@ -49,6 +50,9 @@ public final class Main extends JavaPlugin {
         // Console error engine
         eng = new ConsoleEngine(this);
         this.getEngine().hideConsoleMessages();
+
+        // TPS DETECTOR
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new LagManager(), 100L, 1L);
     }
 
     @Override
@@ -182,5 +186,9 @@ public final class Main extends JavaPlugin {
             return null;
         }
         return getConfig().getStringList(key);
+    }
+
+    public double getTPS() {
+        return LagManager.getTPS();
     }
 }
--- src/main/java/cz/craftmania/crafttweaks/crafttweaks/listeners/CreatureEntityLimiterListener.java
+++ src/main/java/cz/craftmania/crafttweaks/crafttweaks/listeners/CreatureEntityLimiterListener.java
@@ -1,32 +1,91 @@
 package cz.craftmania.crafttweaks.crafttweaks.listeners;
 
+import cz.craftmania.crafttweaks.crafttweaks.Main;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 
+import java.util.Random;
+
 public class CreatureEntityLimiterListener implements Listener {
+
+    private Random randObj = new Random();
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onCreatureSpawn(final CreatureSpawnEvent event) {
         Entity entity = event.getEntity();
         CreatureSpawnEvent.SpawnReason spawnReason = event.getSpawnReason();
 
-        if (entity.getWorld().getEntities().size() >= 1500) { // LIMIT 1500 entit?
-            if (spawnReason == CreatureSpawnEvent.SpawnReason.DEFAULT
-                    || spawnReason == CreatureSpawnEvent.SpawnReason.NATURAL
-                    || spawnReason == CreatureSpawnEvent.SpawnReason.SPAWNER
-                    || spawnReason == CreatureSpawnEvent.SpawnReason.EGG) {
+
+        if (spawnReason == CreatureSpawnEvent.SpawnReason.DEFAULT
+                || spawnReason == CreatureSpawnEvent.SpawnReason.NATURAL
+                || spawnReason == CreatureSpawnEvent.SpawnReason.SPAWNER
+                || spawnReason == CreatureSpawnEvent.SpawnReason.EGG
+                || spawnReason == CreatureSpawnEvent.SpawnReason.NETHER_PORTAL) {
+
+            if (entity.getWorld().getEntities().size() >= 1000) {
                 event.setCancelled(true);
-                return;
+            }
+
+            int tps = (int) Main.getInstance().getTPS();
+            int randNum = randObj.nextInt(100) + 1;
+
+            if (tps >= 20) {
+                event.setCancelled(false);
+            } else if (tps >= 19) {
+                if (!(randNum >= 10)) {
+                    event.setCancelled(true);
+                }
+            } else if (tps >= 18) {
+                if (!(randNum >= 20)) {
+                    event.setCancelled(true);
+                }
+            } else if (tps >= 17) {
+                if (!(randNum >= 30)) {
+                    event.setCancelled(true);
+                }
+            } else if (tps >= 16) {
+                if (!(randNum >= 40)) {
+                    event.setCancelled(true);
+                }
+            } else if (tps >= 15) {
+                if (!(randNum >= 50)) {
+                    event.setCancelled(true);
+                }
+            } else if (tps >= 14) {
+                if (!(randNum >= 60)) {
+                    event.setCancelled(true);
+                }
+            } else if (tps >= 13) {
+                if (!(randNum >= 70)) {
+                    event.setCancelled(true);
+                }
+            } else if (tps >= 12) {
+                if (!(randNum >= 80)) {
+                    event.setCancelled(true);
+                }
+            } else if (tps >= 11) {
+                if (!(randNum >= 90)) {
+                    event.setCancelled(true);
+                }
+            } else {
+                event.setCancelled(true);
             }
         }
 
-        // Zrušení spawnu pigman
-        if (spawnReason == CreatureSpawnEvent.SpawnReason.NETHER_PORTAL) {
-            event.setCancelled(true);
-        }
+        /*if (entity.getWorld().getEntities().size() >= 1000) { // LIMIT 1500 entit?
+            if (spawnReason == CreatureSpawnEvent.SpawnReason.DEFAULT
+                    || spawnReason == CreatureSpawnEvent.SpawnReason.NATURAL
+                    || spawnReason == CreatureSpawnEvent.SpawnReason.SPAWNER
+                    || spawnReason == CreatureSpawnEvent.SpawnReason.EGG
+                    || spawnReason == CreatureSpawnEvent.SpawnReason.NETHER_PORTAL) {
 
+
+
+                event.setCancelled(true);
+            }
+        }*/
     }
 }
